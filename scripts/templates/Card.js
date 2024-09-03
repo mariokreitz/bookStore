7fc2b0381c15bfbb1d3399471504a8f9eb9ad1af
Mario Kreitz <Mario.Kreitz@web.de>
2024-09-03 09:01:26 +0200

    Update Card.js

--- scripts/templates/Card.js
+++ scripts/templates/Card.js
@@ -73,9 +73,46 @@ export default function Card(book, HeartEmpty, HeartFilled, StarEmpty, StarFille
             </table>
         </div>
         <div class="card-footer">
-            <h3>Communiy Reviews</h3>
+            <h3 class="card-title lato-bold">Community Reviews</h3>
             <div class="horizontal-line"></div>
-
+            <div class="reviews-container">
+                ${book.comments
+                  .map((comment) => {
+                    return /*html*/ `
+                      <div class="comment">
+                        <div class="comment-header">
+                            <img class="comment-image" src="https://via.placeholder.com/250" alt="Placeholder">
+                            <div class="comment-info">
+                                <p class="comment-name lato-bold">${
+                                  comment.author
+                                } <span class="comment-rating lato-regular">Rated it</span><span>${Array(5)
+                      .fill(0)
+                      .map((_) => `<span class="star filled">${StarFilled}</span>`)
+                      .join("")}</span></p>
+                                <p class="comment-date">${comment.date}</p>
+                            </div>
+                        </div>
+                        <p class="comment-text">${comment.text}</p>
+                      </div>
+                    `;
+                  })
+                  .join("")}
+            </div>
+            <div class="add-review-container">
+                <h3 class="card-title lato-bold">Add Your Review</h3>
+                <div class="horizontal-line"></div>
+                <form class="add-review-form">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" id="name" class="form-control" name="name" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="comment">Comment</label>
+                        <textarea id="comment" rows="5" cols="30" class="form-control" name="comment" required></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
         </div>
     </div>
   `;
